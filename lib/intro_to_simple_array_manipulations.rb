43a2b28ec66692f333d306fb5560d1b58a97b9c5
Amy Westcott <amy.westcott@yahoo.com>
2019-06-01 21:57:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,8 +11,8 @@ def using_shift(array)
   array.shift 
 end
 def pop_with_args(array)
-  using_pop(array)
-  using_pop(array)
+  using_pop(array, 2)
+  
 end
 def shift_with_args(array)
   using_shift(array, element)
