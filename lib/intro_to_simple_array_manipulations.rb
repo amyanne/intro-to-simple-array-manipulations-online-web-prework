63ed75425424cb144b43c90851feedfda2956964
Amy Westcott <amy.westcott@yahoo.com>
2019-06-01 21:32:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,12 @@
-def using_push(array)
-  array.push("")
+def using_push(array,string)
+  array.push
+end
+def using_unshift(array,string)
+  array.unshift
+end
+def using_pop(array)
+  array.pop
+end
+def using_shift(array)
+  array.shift 
 end
